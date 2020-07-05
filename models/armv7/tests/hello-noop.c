a11a314c248e5366717f4af381cbf9966b9a5d19
Silver <naksitrall@hotmail.co.uk>
2020-07-05 16:01:09 +0100

    Change cp number in noop instr

--- models/armv7/tests/hello-noop.c
+++ models/armv7/tests/hello-noop.c
@@ -4,11 +4,13 @@ int main(int argc, char **argv) {
     printf("Hello!\n");
 
     asm(
-        "mrc    p4, #0, r0, cr0, cr0, #0 \n"
+        "mrc    p2, #0, r0, cr0, cr0, #0 \n"
     );
 
     return 0;
 }
 
-//  arm-linux-gnueabihf-gcc -o ??? -march=armv5 -static ???/arm-hello-noop
-//  arm-linux-gnueabihf-gcc -o ??? -march=armv7 -static ???/arm-hello-noop
+
+//  arm-linux-gnueabi-gcc -o ??? -march=armv7 -static ???/arm-hello-noop
+
+//  arm-linux-gnueabi-gcc -o ??? -marm -static ???/arm-hello-noop
